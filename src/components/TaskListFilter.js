5a52f2a63222f83b6f14aa1855f52a890d419ac9
Rasamune <cjaeger.ld@gmail.com>
2022-04-27 14:50:23 -0400

    Added Filter View Option for mobile

--- src/components/TaskListFilter.js
+++ src/components/TaskListFilter.js
@@ -3,6 +3,8 @@ import classes from './TaskListFilter.module.css';
 
 const TaskListFilter = props => {
   const filters = props.filters;
+  const [showFiltersMobile, setShowFiltersMobile] = useState('');
+  const isMobileView = window.innerWidth < 768;
   const [dropdown, setDropdown] = useState({
     sort: false,
     progress: false,
@@ -34,107 +36,131 @@ const TaskListFilter = props => {
     }));
   };
 
+  const toggleVisiblityHandler = e => {
+    if (showFiltersMobile === '') {
+      e.target.textContent = 'Close Sorting Filters';
+      setShowFiltersMobile('show');
+      return;
+    }
+    e.target.textContent = 'View Sorting Filters';
+    setShowFiltersMobile('');
+  };
+
   return (
-    <section className={classes['task-filter-container']}>
-      <div className={classes.sort}>
-        <label>Sort:</label>
-        <div
-          className={`${classes.filterchoice} ${
-            dropdown.sort && classes.selected
-          }`}
-          onClick={toggleListHandler}
-          onMouseLeave={closeListHandler}
-          data-filter="sort"
-        >
-          <span>{filters.time.name}</span>
-          {dropdown.sort && (
-            <ul
-              className={classes.filterlist}
-              id="time"
-              onClick={clickFilterHandler}
-            >
-              <li data-value="new">Newest</li>
-              <li data-value="update">Last Updated</li>
-              <li data-value="dueDate">Due Date</li>
-              <li data-value="old">Oldest</li>
-            </ul>
-          )}
+    <>
+      <section
+        className={`${classes['task-filter-container']} ${
+          showFiltersMobile === 'show' ? classes.show : ''
+        }`}
+      >
+        <div className={classes.sort}>
+          <label>Sort:</label>
+          <div
+            className={`${classes.filterchoice} ${
+              dropdown.sort && classes.selected
+            }`}
+            onClick={toggleListHandler}
+            onMouseLeave={closeListHandler}
+            data-filter="sort"
+          >
+            <span>{filters.time.name}</span>
+            {dropdown.sort && (
+              <ul
+                className={classes.filterlist}
+                id="time"
+                onClick={clickFilterHandler}
+              >
+                <li data-value="new">Newest</li>
+                <li data-value="update">Last Updated</li>
+                <li data-value="dueDate">Due Date</li>
+                <li data-value="old">Oldest</li>
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-      <div className={classes.sort}>
-        <label>Progress:</label>
-        <div
-          className={`${classes.filterchoice} ${
-            dropdown.progress && classes.selected
-          }`}
-          onClick={toggleListHandler}
-          onMouseLeave={closeListHandler}
-          data-filter="progress"
-        >
-          <span>{filters.progress.name}</span>
-          {dropdown.progress && (
-            <ul
-              className={classes.filterlist}
-              id="progress"
-              onClick={clickFilterHandler}
-            >
-              <li data-value="all">All Tasks</li>
-              <li data-value="new">Not Started</li>
-              <li data-value="inprogress">In Progress</li>
-              <li data-value="completed">Completed</li>
-            </ul>
-          )}
+        <div className={classes.sort}>
+          <label>Progress:</label>
+          <div
+            className={`${classes.filterchoice} ${
+              dropdown.progress && classes.selected
+            }`}
+            onClick={toggleListHandler}
+            onMouseLeave={closeListHandler}
+            data-filter="progress"
+          >
+            <span>{filters.progress.name}</span>
+            {dropdown.progress && (
+              <ul
+                className={classes.filterlist}
+                id="progress"
+                onClick={clickFilterHandler}
+              >
+                <li data-value="all">All Tasks</li>
+                <li data-value="new">Not Started</li>
+                <li data-value="inprogress">In Progress</li>
+                <li data-value="completed">Completed</li>
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-      <div className={classes.sort}>
-        <label>Priority:</label>
-        <div
-          className={`${classes.filterchoice} ${
-            dropdown.priority && classes.selected
-          }`}
-          onClick={toggleListHandler}
-          onMouseLeave={closeListHandler}
-          data-filter="priority"
-        >
-          <span>{filters.priority.name}</span>
-          {dropdown.priority && (
-            <ul
-              className={classes.filterlist}
-              id="priority"
-              onClick={clickFilterHandler}
-            >
-              <li data-value="all">Any Priority</li>
-              <li data-value="high">High</li>
-              <li data-value="medium">Medium</li>
-              <li data-value="low">Low</li>
-            </ul>
-          )}
+        <div className={classes.sort}>
+          <label>Priority:</label>
+          <div
+            className={`${classes.filterchoice} ${
+              dropdown.priority && classes.selected
+            }`}
+            onClick={toggleListHandler}
+            onMouseLeave={closeListHandler}
+            data-filter="priority"
+          >
+            <span>{filters.priority.name}</span>
+            {dropdown.priority && (
+              <ul
+                className={classes.filterlist}
+                id="priority"
+                onClick={clickFilterHandler}
+              >
+                <li data-value="all">Any Priority</li>
+                <li data-value="high">High</li>
+                <li data-value="medium">Medium</li>
+                <li data-value="low">Low</li>
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-      <div className={classes.sort}>
-        <label>View:</label>
-        <div
-          className={`${classes.filterchoice} ${
-            dropdown.view && classes.selected
-          }`}
-          onClick={toggleListHandler}
-          onMouseLeave={closeListHandler}
-          data-filter="view"
-        >
-          <span>{filters.view.name}</span>
-          {dropdown.view && (
-            <ul
-              className={classes.filterlist}
-              id="view"
-              onClick={clickFilterHandler}
-            >
-              <li data-value="expand">Expanded</li>
-              <li data-value="minimize">Minimized</li>
-            </ul>
-          )}
+        <div className={classes.sort}>
+          <label>View:</label>
+          <div
+            className={`${classes.filterchoice} ${
+              dropdown.view && classes.selected
+            }`}
+            onClick={toggleListHandler}
+            onMouseLeave={closeListHandler}
+            data-filter="view"
+          >
+            <span>{filters.view.name}</span>
+            {dropdown.view && (
+              <ul
+                className={classes.filterlist}
+                id="view"
+                onClick={clickFilterHandler}
+              >
+                <li data-value="expand">Expanded</li>
+                <li data-value="minimize">Minimized</li>
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {isMobileView && (
+        <button
+          className={classes['view-filters']}
+          onClick={toggleVisiblityHandler}
+        >
+          View Sorting Filters
+        </button>
+      )}
+    </>
   );
 };
 
